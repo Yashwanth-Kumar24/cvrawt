314cfefc661e39554fefec01b281946c1b013738
Yashwanth-Kumar24 <yashwanthkumar.jannaikode@gmail.com>
2021-06-17 19:24:29 +0530

    react new

---
+++ react/react2/src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                Hey hello
+            </div>  
+        )
+    }
+}
+
+ReactDOM.render(<Index/>,document.getElementById('root'))
